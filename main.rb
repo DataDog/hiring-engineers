d4a0830711e2bfb38c2fb31e55a905b15aa2cbe4
jbmilgrom <jbmilgrom@gmail.com>
2014-08-12 16:15:07 -0400

    able to sent event with email notification

--- main.rb
+++ main.rb
@@ -3,5 +3,5 @@ require 'pry'
 
 dog = Dogapi::Client.new(ENV["DATADOG_API_KEY"])
 
-dog.emit_event(Dogapi::Event.new('cool this works', :msg_title => 'Test2'))
+dog.emit_event(Dogapi::Event.new('@jbmilgrom@gmail.com - nice! this works', :msg_title => 'Test3'))
 
