3d526663a706ec9d6d1112281f2c67f4ca53b9ed
jkrth617 <jkrth617@gmail.com>
2015-12-11 10:32:00 -0500

    fix img in readme

--- question_one.rb
+++ question_one.rb
@@ -4,7 +4,7 @@ require 'dogapi'
 api_key='822c15b7b2f9fb8c49e8827404903e58'
 app_key='f1dab6bb368ffa58b5d5af3358d77e42f48a1a21'
 
-dog = Dogapi::Client.new(api_key)
+dog = Dogapi::Client.new(api_key, app_key)
+host_name = 'vagrant-ubuntu-trusty-64'
 
-#include the email address with an '@' infront of it in the message of the event to have it send the event to that email address
 test_event = dog.emit_event(Dogapi::Event.new("this will be the text\n@jroth@colgate.edu", :msg_title => 'My Title :)'))
