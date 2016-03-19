e193ab32ecfa50c757bc4d697bd26c3de6befbd6
Matt Bryan <mpbryan90@gmail.com>
2016-03-19 16:25:53 -0400

    added node_modules to .gitignore

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const dogapi = require("dogapi");
 const secrets = require("./secrets");
 
-//this package has a few advantages over the one in the Datadog docs:
-//1. it's published so can be installed from the command line with npm
-//2. it has much more functionality
-//3. it has a command line interface itself!
-//it can be found at https://www.npmjs.com/package/dogapi
-
-//secrets.js is in .gitignore so that my api/app keys aren't exposed on github
 const options = {
 	api_key: secrets.apiKey,
 	app_key: secrets.appKey
