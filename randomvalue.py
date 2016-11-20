bfbc85d85675954b4a36b9805f7085c503354a9c
rberlind <rberlind@optonline.net>
2016-11-20 14:58:46 -0500

    Fixed checking of yaml file

--- randomvalue.py
+++ randomvalue.py
@@ -14,5 +14,10 @@ last = self.init_config.get('last', 100)
 
 class MyRandomValueCheck(AgentCheck):
     def check(self, instance):
-        self.gauge('random.value', random.randint(first, last)):
-        self.log.info("Just set a gauge to a random integer between the first and last values in the check configuration file."):
+        # Load values from the randomvalue.yaml file
+        first = self.init_config.get('first', 1)
+        last = self.init_config.get('last', 100)
+
+        # Generate the random value and log that we did it
+        self.gauge('random.value', random.randint(first, last))
+        self.log.info("Just set a gauge to a random integer between the first and last values in the check configuration file.")
