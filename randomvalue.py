a3c14db9b0a4e9ceae8dafe9791e9fab023fe9bb
rberlind <rberlind@optonline.net>
2016-11-20 14:46:55 -0500

    A check that sends a random integer

---
+++ randomvalue.py
@@ -0,0 +1,18 @@
+# By Roger Berlind
+
+# project
+from config import _is_affirmative
+from checks import AgentCheck
+from util import Platform
+
+# stdlib
+import random
+
+# Load values from the check config file
+first = self.init_config.get('first', 1)
+last = self.init_config.get('last', 100)
+
+class MyRandomValueCheck(AgentCheck):
+    def check(self, instance):
+        self.gauge('random.value', random.randint(first, last)):
+        self.log.info("Just set a gauge to a random integer between the first and last values in the check configuration file."):
