69dfc71bbf02097c208c7320b2a79d0f688bc6e5
bweinstein <bradleyseth.weinstein@gmail.com>
2017-05-15 14:54:48 -0700

    randomcheck gauge

--- checks.d/randomcheck.py
+++ checks.d/randomcheck.py
@@ -3,4 +3,10 @@ import random
 from checks import AgentCheck
 class RandomCheck(AgentCheck):
     def check(self, instance):
-        print('test.support.random', random.random())
+    #    print('test.support.random', random.random())
+    #     self.service_check('test.support.random', message='test.support.random:', random.random())
+    # currentRandom=random.random()
+    
+     self.gauge('test.support.random', random.random())
+
+
