1f73699d509690439df6fe8a9d7a4f7899673468
pat mcb <pmcbrien@users.noreply.github.com>
2017-08-09 15:39:41 -0400

    Update vagrant-install-lamp-ddagent.sh

--- vagrant-install-lamp-ddagent.sh
+++ vagrant-install-lamp-ddagent.sh
@@ -6,10 +6,9 @@ vagrant init hashicorp/precise64
 vagrant up
 
 vagrant ssh -c 'sudo apt-get -y update'
-vagrant ssh -c 'sudo apt-get -y install apache2 mysql'
-vagrant ssh -c 'sudo apt-get install mysql-server'
+vagrant ssh -c 'sudo apt-get -y install curl apache2 mysql'
+vagrant ssh -c 'sudo apt-get -y install mysql-server mongodb'
 vagrant ssh -c 'sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt'
 vagrant ssh -c 'DD_API_KEY=API_KEY_GOES_HERE bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"'
 
 vagrant ssh
-
