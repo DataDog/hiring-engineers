6b64c1c08df96879f0cde903a8491bfafb2779b9
s.guerrier <s.guerrier@criteo.com>
2017-12-02 11:15:48 +0100

    changed implementation of the script from ScreenBoard to TimeBoard

--- update_board.py
+++ update_board.py
@@ -2,30 +2,33 @@ import argparse
 import json
 from datadog import initialize, api
 
-#id = 249100
+id = 249100
 
 def check_board_presence(id):
-  result = api.Screenboard.get(id)
+  result = api.Timeboard.get(id)
   if ("errors" in result):
     print("Board not found.")
     return False
   print("Board found.")
   return True
 
-def create_board(board_json):
-  result = api.Screenboard.create(**board_json)
+def create_board(title, board_json):
+  description = "my board"
+  result = api.Timeboard.create(title=title, description=description, graphs=board_json, read_only=True)
   #Print ID of the board
-  print(result['id'])
+  print(result)
 
-def update_board(board_id, board_json):
-  result = api.Screenboard.update(board_id, **board_json)
+def update_board(board_id, title, board_json):
+  description = "my board"
+  result = api.Timeboard.update(board_id, title=title, description=description, graphs=board_json, read_only=True)
   #Print ID of the board
-  print(result['id'])
+  print(result)
 
 def main():
 
   parser = argparse.ArgumentParser(description='Parse board name and datadog api info')
   parser.add_argument('--id', type=int, required=True)
+  parser.add_argument('--name', required=True)
   parser.add_argument('--apikey', required=True)
   parser.add_argument('--appkey', required=True)
   parser.add_argument('--json', required=True)
@@ -46,9 +49,9 @@ def main():
 
   #Create or Update depending on the reuslt of the last check
   if (board_exists):
-    update_board(args.id, board_json)
+    update_board(args.id, args.name, board_json)
   else:
-    create_board(board_json)
+    create_board(args.name, board_json)
 
 if __name__ == "__main__" :
   main()
