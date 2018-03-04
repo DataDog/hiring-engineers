11e7f9452458e8d8f9ba8b5a5127794ed427b7f0
draav <devlin.nicholasj@gmail.com>
2018-03-04 00:29:47 -0500

    install agent with vagrant bootstrap file

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,6 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "hashicorp/precise64"
+  
+  config.vm.provision :file, source: ".env", destination: ".env"
+  config.vm.provision :shell, path: "bootstrap.sh"
 end
---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "bootstrap.sh 1: allow apt to install through https"
+sudo apt-get update
+sudo apt-get install apt-transport-https
+
+echo "bootstrap.sh 2: set up the Datadog deb repo on system and import Datadog's apt key"
+sudo sh -c "echo 'deb https://apt.datadoghq.com/ stable 6' > /etc/apt/sources.list.d/datadog.list"
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 382E94DE
+
+echo "bootstrap.sh 3: install the Agent"
+sudo apt-get update
+sudo apt-get install datadog-agent
+
+echo "bootstrap.sh 4: copy the example config and plug in API key from .env"
+source .env
+echo "api key: $DATADOG_API_KEY"
+sudo sh -c "sed 's/api_key:.*/api_key: $DATADOG_API_KEY/' /etc/datadog-agent/datadog.yaml.example > /etc/datadog-agent/datadog.yaml"
+
+echo "boostrap.sh 5: start the datadog agent"
+sudo initctl start datadog-agent
