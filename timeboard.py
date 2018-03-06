fbd9edf19f5322a20783b7a14786fc4d88450f3e
draav <devlin.nicholasj@gmail.com>
2018-03-06 02:37:13 -0500

    create shell script that uses datadog api to create timeboard

---
+++ timeboard.py
@@ -0,0 +1,75 @@
+from datadog import initialize, api
+
+options = {
+    'api_key': '***REMOVED***',
+    'app_key': '***REMOVED***'
+}
+
+initialize(**options)
+
+title = "My Timeboard"
+description = "An informative timeboard."
+graphs = [{
+    "definition": {
+        "events": [],
+        "requests": [
+            {
+                "q": "avg:random.number{*}",
+                "type": "line",
+                "style": {
+                    "palette": "dog_classic",
+                    "type": "solid",
+                    "width": "normal"
+                },
+                "conditional_formats": [],
+                "aggregator": "avg"
+            },
+            {
+                "q": "anomalies(avg:postgresql.connections{*}, 'basic', 2)",
+                "type": "line",
+                "style": {
+                    "palette": "dog_classic",
+                    "type": "solid",
+                    "width": "normal"
+                }
+            },
+            {
+                "q": "avg:random.number{*}.rollup(sum, 300)",
+                "type": "line",
+                "style": {
+                    "palette": "dog_classic",
+                    "type": "solid",
+                    "width": "normal"
+                }
+            }
+        ],
+        "viz": "timeseries"
+    },
+    "title": "Random Number Metrics"
+    },
+    {
+    "definition": {
+        "events": [],
+        "requests": [
+            {
+                "q": "anomalies(avg:postgresql.db.count{*}, 'basic', 2)",
+                "type": "line",
+                "style": {
+                    "palette": "dog_classic",
+                    "type": "solid",
+                    "width": "normal"
+                }
+            }
+        ],
+        "viz": "timeseries"
+    },
+    "title": "Database connections anomalies"
+}]
+
+read_only = True
+result = api.Timeboard.create(title=title,
+                     description=description,
+                     graphs=graphs,
+                     read_only=read_only)
+
+print(result)
