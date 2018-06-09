b63ed6ebd6d6cb37068a21357e16feff3edcdcc3
Ben Basuni <basuni91@hotmail.com>
2018-06-09 16:02:17 -0700

    edited database

--- config/config.py
+++ config/config.py
@@ -1,2 +1,5 @@
-SQLALCHEMY_DATABASE_URI="postgresql://datadog@localhost/datadog_db"
+import os
+
+#SQLALCHEMY_DATABASE_URI="postgresql://datadog@localhost/datadog_db"
+SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS=True
