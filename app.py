9c3d057189c0b93e112061276fca12a0a151478e
Ben Basuni <basuni91@hotmail.com>
2018-06-08 23:59:06 -0700

    modified app py for production

--- app.py
+++ app.py
@@ -46,7 +46,9 @@ def weather():
 #####################
 ## Run Application ##
 ######################
-app.run(debug=True, port=8000, host="0.0.0.0")
+if __name__ == '__main__':
+    #app.run(debug=True, port=8000, host="0.0.0.0")
+    app.run()
 
 
 # Try and Catch
