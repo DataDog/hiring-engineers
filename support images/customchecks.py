4a43255c216fb88888dec4d4b1a93fb7a67d7653
josephrivers <41809772+josephrivers@users.noreply.github.com>
2018-07-28 11:33:09 -0400

    Delete customchecks.py

--- support images/customchecks.py
+++
@@ -1,6 +0,0 @@
-from datadog_checks.checks import AgentCheck
-from random import randrange
-
-class RandomCheck(AgentCheck):
-    def check(self, instance):
-		self.gauge('my_metric', randrange(0, 1000))
