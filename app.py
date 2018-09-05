46de7d46bb26f964944775d24bd75d2c41d283f0
james smith <noletubby77@hotmail.com>
2018-09-05 19:49:33 -0400

    python app for collecting APM data

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+import blinker as _
+import logging
+import sys
+
+
+# Datadog middleware for Flask
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
+# Have flask use stdout as the logger
+main_logger = logging.getLogger()
+main_logger.setLevel(logging.DEBUG)
+c = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c.setFormatter(formatter)
+main_logger.addHandler(c)
+
+app = Flask(__name__)
+
+# Simple trace middleware object 
+traced_app = TraceMiddleware(app, tracer, service="my-flask-app", distributed_tracing=False)
+
+
+@app.route('/')
+def api_entry():
+    return 'Entrypoint to the Application'
+
+@app.route('/api/apm')
+def apm_endpoint():
+    return 'Getting APM Started'
+
+@app.route('/api/trace')
+def trace_endpoint():
+    return 'Posting Traces'
+
+if __name__ == '__main__':
+    app.run(host='172.18.10.10', port='8080')
