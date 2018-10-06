28f4c0d53172a2cdd0768c68a4de51ce278dfce7
Abdullah Khan <ajkhan4096@gmail.com>
2018-10-06 19:42:12 -0400

    Add my_metric.py
    Python AgentCheck which submits a metric
    with a random value between [0,1000].

---
+++ checks_files/my_metric.py
@@ -0,0 +1,11 @@
+from checks import AgentCheck
+from random import SystemRandom
+
+# HelloCheck inherits from AgentCheck
+class my_metric_check(AgentCheck):
+    '''
+    A custom check that submits a metric named "my_metric" with a random value
+    between 0 and 1000.
+    '''
+    def check(self, instance):
+        self.gauge('my_metric', SystemRandom().randint(0,1000), tags=['rng'])
