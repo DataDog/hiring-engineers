fd0f9885c0e6de0d2ced1e27daa357862621720a
Albertson, Arlington <Arlington.J.Albertson@nordstrom.com>
2018-10-11 03:05:33 -0700

    adding instrumented apm flask app

---
+++ homework-assets/apm-app/apm_flask_app.py
@@ -0,0 +1,33 @@
+"""This application has a basic hello World response
+    as well as handles monitoring components"""
+from flask import Flask
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+import logging
+import sys
+
+# Have flask use stdout as the logger
+main_logger = logging.getLogger()
+main_logger.setLevel(logging.DEBUG)
+c = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c.setFormatter(formatter)
+main_logger.addHandler(c)
+
+app = Flask(__name__)
+traced_app = TraceMiddleware(app, tracer, service="apm_flask_app", distributed_tracing=True)
+
+@app.route('/')
+def api_entry():
+    return 'Entrypoint to the Application!'
+
+@app.route('/api/apm')
+def apm_endpoint():
+    return 'Getting APM Started'
+
+@app.route('/api/trace')
+def trace_endpoint():
+    return 'Posting Traces'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5050')
