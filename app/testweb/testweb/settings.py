5e937148c202ae0399ad2917fc1e27cf485cf0f6
Yuya <u4.yi3k8a@gmail.com>
2018-10-20 01:06:14 +0900

    Custom app for test use

--- app/testweb/testweb/settings.py
+++ app/testweb/testweb/settings.py
@@ -92,7 +92,7 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["cloud-steps.com", "localhost"]
+ALLOWED_HOSTS = ["cloud-steps.com", "localhost", "testweb.cloud-steps.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
