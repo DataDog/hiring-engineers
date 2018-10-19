aa81bb7cc0bd431ee0c3d204c5087188668844aa
Rui Lamy <ruilamy@Ruis-MacBook-Pro.local>
2018-10-19 14:52:21 +0100

    Final version

--- python/mycheck.py
+++ python/mycheck.py
@@ -6,12 +6,3 @@ from random import randint
 class my_metricCheck(AgentCheck):
     def check(self, instance):
         self.gauge('my_metric', randint(0, 1000))
-
-
-        def check(self, instance):
-    if time.time() - self.last_collection < 5 * 60: # 5 minutes 
-        self.log.debug("Check ran less than 5 min ago, not running it")
-        return
-
-    self.last_collection = time.time()
-    self.actually_process_check(instance)
