ec5c88a5a80b133ad6e669d9d125b46375afdeff
simuvid <christian@glatschke.com>
2018-11-10 00:10:01 +0100

    Update logfather_tb.py

--- code/logfather_tb.py
+++ code/logfather_tb.py
@@ -4,8 +4,8 @@ from datadog import initialize, api
 options = {
     # Find your keys here:
     # https://app.datadoghq.com/account/settings#api
-    'api_key': '719d714d7132af72ce6e1f2d8b67b618',
-    'app_key': 'ad4336c11c87bab29da8da02b46921f83314c922'
+    'api_key': '****************************************',
+    'app_key': '****************************************'
 }
 
 initialize(**options)
