67b72e3b94a436cb08f14c14d415dcb56219200d
Jon Barker <jonathan.t.barker@gmail.com>
2018-11-11 15:14:19 -0600

    Update timeboard.py

--- timeboard.py
+++ timeboard.py
@@ -1,34 +1,50 @@
 from datadog import initialize, api
 
 options = {
-    'api_key': '<YOUR_API_KEY>',
-    'app_key': '<YOUR_APP_KEY>'
+    'api_key': '16ff05c7af6ed4652a20f5a8d0c609ce',
+    'app_key': 'e6a169b9b337355eef90002878fbf9a565e9ee77'
 }
 
 initialize(**options)
 
-title = "My Timeboard"
-description = "An informative timeboard."
-graphs = [{
-    "definition": {
-        "events": [],
-        "requests": [
-            {"q": "avg:system.mem.free{*}"}
-        ],
-        "viz": "timeseries"
+title = "Mymetric timeboard"
+description = "Mymetric Timeboard"
+graphs = [
+    {
+        "definition": {
+            "events": [],
+            "requests": [
+                {"q": "avg:mymetric{host:ubuntu-xenial}"}
+            ],
+            "viz": "timeseries"
+        },
+        "title": "mymetric in timeseries"
     },
-    "title": "Average Memory Free"
-}]
-
+    {
+        "definition": {
+            "events": [],
+            "requests": [
+                {"q": "anomalies(avg:postgres.connections.current{host:ubuntu-xenial}, 'basic', 2)"}
+            ],
+            "viz": "timeseries"
+        },
+        "title": "PostgreSQL connections"
+    },
+    {
+        "definition": {
+            "events": [],
+            "requests": [
+                {"q": "avg:mymetric{host:ubuntu-xenial}.rollup(sum, 3600)"}
+            ],
+            "viz": "timeseries"
+        },
+        "title": "Rollup function mymetric"
+    },
+ ]
 template_variables = [{
-    "name": "host1",
+    "name": "ubuntu_xenial",
     "prefix": "host",
     "default": "host:my-host"
 }]
-
 read_only = True
-api.Timeboard.create(title=title,
-                     description=description,
-                     graphs=graphs,
-                     template_variables=template_variables,
-                     read_only=read_only)
+api.Timeboard.create(title=title,description=description,graphs=graphs,template_variables=template_variables)
