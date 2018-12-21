02221bb3b5d05a3c97237092f3ab0f61f0d65aaa
weevilkris <43314604+weevilkris@users.noreply.github.com>
2018-12-20 23:36:18 -0500

    Create customcheck.py

---
+++ sample_code/customcheck.py
@@ -0,0 +1,15 @@
+try:
+    # first, try to import the base class from old versions of the Agent...
+    from checks import AgentCheck
+except ImportError:
+    # ...if the above failed, the check is running in Agent version 6 or later
+    from datadog_checks.checks import AgentCheck
+# content of the special variable __version__ will be shown in the Agent status page
+__version__ = "1.0.0"
+
+import random
+
+class CustomtestCheck(AgentCheck):
+    def check(self, instance):
+        myvalue = random.randint(0,1001)
+        self.gauge('customtest.my_metric', myvalue)
