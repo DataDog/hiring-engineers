0331a9caa5c744a874ad8524901df034dde9e6f2
Dipankar BARUA <barua.dipankar04@gmail.com>
2019-01-04 15:31:15 +0100

    custom dashboard via api

---
+++ File Folder/Custom_dashboard.py
@@ -0,0 +1,27 @@
+initialize(**options)
+
+title = "Dipankar Barua Time Board"
+description = "An informative timeboard."
+graphs = [{
+    "definition": {
+        "events": [],
+        "requests": [
+            {"q": "avg:system.mem.free{*}"}
+        ],
+        "viz": "timeseries"
+    },
+    "title": "Average Memory Free"
+}]
+
+template_variables = [{
+    "name": "host1",
+    "prefix": "host",
+    "default": "host:my-host"
+}]
+
+read_only = True
+api.Timeboard.create(title=title,
+                     description=description,
+                     graphs=graphs,
+                     template_variables=template_variables,
+                     read_only=read_only)
