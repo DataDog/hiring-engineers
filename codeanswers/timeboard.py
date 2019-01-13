b9e4b12221c5563704feba3329929dff1c7e678f
Flatiron School <flatironschool@Flatirons-MacBook-Air-8.local>
2019-01-13 12:56:23 -0500

    added metrics and rollup and graphs

--- codeanswers/timeboard.py
+++ codeanswers/timeboard.py
@@ -4,3 +4,55 @@ options = {
     'api_key': 'c4dc0b5c7063b9e0fc14131c918b5e32',
     'app_key': '685db677bb60dbbe589ad55481daf531baf62500'
 }
+
+
+initialize(**options)
+
+title = "Timeboard and Metric"
+description = "A timeboard displaying metric data"
+graphs = [
+    {
+        "definition": {
+            "events": [],
+            "requests": [
+                {"q": "avg:my_metric{host:vagrant}"}
+            ],
+            "viz": "timeseries"
+        },
+        "title": "My Metric"
+    },
+    {
+        "definition": {
+            "events": [],
+            "requests": [
+                {"q": "anomalies(avg:postgresql.max_connections{host:vagrant}, 'basic', 2)"}
+            ],
+            "viz": "timeseries"
+        },
+        "title": "PostgreSQL Anomaly"
+    },
+    {
+        "definition": {
+            "events": [],
+            "requests": [
+                {"q": "avg:my_metric{host:vagrant}.rollup(sum, 3600)"}
+            ],
+            "viz": "timeseries"
+        },
+        "title": "Rollup"
+    },
+
+]
+
+template_variables = [{
+    "name": "host1",
+    "prefix": "host",
+    "default": "host:my-host"
+}]
+
+read_only = True
+api.Timeboard.create(title=title,
+                     description=description,
+                     graphs=graphs,
+                     template_variables=template_variables,
+                     read_only=read_only)
