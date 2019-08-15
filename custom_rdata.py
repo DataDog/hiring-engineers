e6627868a52fdac2734dcfe54a8c455fbc1b5111
Sal-arus <53800092+Sal-arus@users.noreply.github.com>
2019-08-15 23:59:28 +1000

    Delete custom_rdata.py

--- custom_rdata.py
+++
@@ -1,7 +0,0 @@
-from checks import AgentCheck
-from random import randint
-__version__ = "1.0.0"
-class rdata(AgentCheck):
-    def check(self, instance):
-        self.gauge('rdata.my_metric', randint(0,1000))
-
