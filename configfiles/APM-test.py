dd800335b3e4e977e0961e4afa9de49410fa713c
wkarges <willkarges@gmail.com>
2020-01-24 14:38:56 -0700

    Final Edits1

---
+++ configfiles/APM-test.py
@@ -0,0 +1,16 @@
+from ddtrace import patch_all; patch_all(logging=True)
+import logging
+from ddtrace import tracer
+
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '[dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+          '- %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger(__name__)
+log.level = logging.INFO
+
+@tracer.wrap('testApp.printstring', service='hiring_exercise-service', resource='testApp-response')
+def testApp():
+    log.info('This is my test app for my DD Hiring exercise!')
+
+testApp()
