9227bd1db1b8a2769400c3efcd92d915460476ea
jack <jp495@users.noreply.github.com>
2020-02-02 21:51:02 -0500

    Cat's out of the bag. (that got revoked)

--- run_datadog_docker.sh
+++ run_datadog_docker.sh
@@ -6,6 +6,6 @@ docker run -d --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro \
                               -v /proc/:/host/proc/:ro \
                               -p 127.0.0.1:8126:8126/tcp \
                               -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
-                              -e DD_API_KEY=87b7c220850533645bf5c6c2da43f91a \
+                              -e DD_API_KEY=XXX \
                               -e DD_APM_ENABLED=true \
                               datadog/agent:7
