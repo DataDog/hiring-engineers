7100bfa1f4bfdcbd763da8c58418ba6c8a6e4194
Christoph A. Torlinsky <christberlin@icloud.com>
2020-02-23 10:34:15 +0100

    Update

--- scripts/timeboard-example.sh
+++ scripts/timeboard-example.sh
@@ -1,5 +1,6 @@
 #api_key=<DATADOG_API_KEY>
 #app_key=<DATADOG_APPLICATION_KEY>
+# replace host with dyno (app server name) and API endpoint with EU or US
 
 curl  -X POST -H "Content-type: application/json" \
 -d '{
