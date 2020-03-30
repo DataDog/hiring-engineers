9263180f57b39ae5faa1d072d22e2ed923a750c7
new_account <sheltowt@gmail.com>
2020-03-30 13:45:27 -0600

    adding apm collection script

--- scripts/custom_timeboard.py
+++ scripts/custom_timeboard.py
@@ -1,7 +1,22 @@
 #the documentation says that the timeboard endpoint is deprecated
 #and that it is best to use the dashboard endpoint
 
+# Utilize the Datadog API to create a Timeboard that contains:
+
+# * Your custom metric scoped over your host.
+# * Any metric from the Integration on your Database with the anomaly function applied.
+# * Your custom metric with the rollup function applied to sum up all the points for the past hour into one bucket
+
+# Please be sure, when submitting your hiring challenge, to include the script that you've used to create this Timeboard.
+
+# Once this is created, access the Dashboard from your Dashboard List in the UI:
+
+# * Set the Timeboard's timeframe to the past 5 minutes
+# * Take a snapshot of this graph and use the @ notation to send it to yourself.
+# * **Bonus Question**: What is the Anomaly graph displaying?
+
 from datadog import initialize, api
+import json
 
 with open("../config.json") as f:
     config = json.load(f)
@@ -13,3 +28,37 @@ options = {
 
 initialize(**options)
 
+
+title = 'Average Memory Free Shell'
+widgets = [{
+    'definition': {
+        'type': 'timeseries',
+        'requests': [
+            {'q': 'avg:system.mem.free{*}'}
+        ],
+        'title': 'Average Memory Free'
+    }
+}]
+layout_type = 'ordered'
+description = 'A dashboard with memory info.'
+is_read_only = True
+notify_list = ['user@domain.com']
+template_variables = [{
+    'name': 'host1',
+    'prefix': 'host',
+    'default': 'my-host'
+}]
+
+saved_view = [{
+    'name': 'Saved views for hostname 2',
+    'template_variables': [{'name': 'host', 'value': '<HOSTNAME_2>'}]}
+]
+
+api.Dashboard.create(title=title,
+                     widgets=widgets,
+                     layout_type=layout_type,
+                     description=description,
+                     is_read_only=is_read_only,
+                     notify_list=notify_list,
+                     template_variables=template_variables,
+                     template_variable_presets=saved_views)
