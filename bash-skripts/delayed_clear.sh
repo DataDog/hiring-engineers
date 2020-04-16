1855b74c9a1fba359f0947d2f99aa688c91efdd0
Lutz Lange <lutzinberlin@gmail.com>
2020-04-16 11:43:02 +0200

    delayed clear update

--- bash-skripts/delayed_clear.sh
+++ bash-skripts/delayed_clear.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# Author : Lutz Lange <lutzinberlin@gmail.com>
+# 
+# This is part of a simple load generator for postgre
+#
+# Run this if you are running the inserts.sh and you want to prevent your disc filling up
 
 while true; do
-  sleep 300
-  mysql -u instanian -pinstant21 sampledb -e'DELETE from checkin;' 2>/dev/null
+  sleep 600
+  psql sampledb -h localhost -c 'DELETE from checkin;' 2>/dev/null
 done
 
