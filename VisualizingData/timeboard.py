14e9147b23534d949071cb2b96e0e67689511a79
rathojohn <john.rath202@gmail.com>
2020-05-27 19:45:22 -0400

    Add files via upload

---
+++ VisualizingData/timeboard.py
@@ -0,0 +1,34 @@
+from datadog import initialize, api
+
+options = {
+    'api_key': '91300d2d9cd0ccf8b22dbd46124c3102',
+    'app_key': '4d6730f87624f71e8dac1817951f3a1d35e0e8d0'
+}
+
+initialize(**options)
+
+title = "My Timeboard"
+description = "An informative timeboard."
+graphs = [{
+    "definition": {
+        "events": [],
+        "requests": [
+            {"q": "avg:system.mem.free{*}"}
+        ],
+        "viz": "timeseries"
+    },
+    "title": "Average Memory Free"
+}]
+
+template_variables = [{
+    "name": "host1",
+    "prefix": "host",
+    "default": "host:my-host"
+}]
+
+read_only = True
+api.Timeboard.create(title=title,
+                     description=description,
+                     graphs=graphs,
+                     template_variables=template_variables,
+                     read_only=read_only)
