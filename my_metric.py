ba83d19d2b1b12d7edbba503079910cd9dc59892
p6steve <40125330+p6steve@users.noreply.github.com>
2020-06-03 19:08:49 +0100

    Create my_metric.py

---
+++ my_metric.py
@@ -0,0 +1,13 @@
+import random
+  
+from datadog_checks.base import AgentCheck
+
+__version__ = "1.0.0"
+
+class MyClass(AgentCheck):
+    def check(self, instance):
+        self.gauge(
+            "my_metric.gauge",
+            random.randint(0, 1000),
+            tags=["env:dev","metric_submission_type:gauge"],
+        )
