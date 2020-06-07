597453e67b0fa835ea5da77524d8eab3b12db240
p6steve <40125330+p6steve@users.noreply.github.com>
2020-06-07 14:58:55 +0100

    Added tracer instrumentation

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 import logging
 import sys
 
+from ddtrace import tracer
+
 # Have flask use stdout as the logger
 main_logger = logging.getLogger()
 main_logger.setLevel(logging.DEBUG)
@@ -13,14 +15,22 @@ main_logger.addHandler(c)
 app = Flask(__name__)
 
 @app.route('/')
+@tracer.wrap(name='my_entry')
 def api_entry():
     return 'Entrypoint to the Application'
 
 @app.route('/api/apm')
+@tracer.wrap(name='my_apm_endpoint')
 def apm_endpoint():
+    with tracer.trace('my_apm_span') as span:
+        #designate this span as a separate service:
+        span.service='my_apm_service'
+        #add metadate
+        span.set_tag('service_type','my_apm_service_type')
     return 'Getting APM Started'
 
 @app.route('/api/trace')
+@tracer.wrap(name='my_apm_trace',service='my_apm_trace_service')
 def trace_endpoint():
     return 'Posting Traces'
 
