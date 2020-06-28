2b11fa5747a9f6bd478aa0b9220131b9cee43bb3
Sean Carolan <scarolan@hashicorp.com>
2020-06-28 14:15:04 -0500

    Update the datadog_setup.sh script.

--- solution/assets/datadog_setup.sh
+++ solution/assets/datadog_setup.sh
@@ -12,16 +12,11 @@ cp /home/ubuntu/bits.txt /etc/motd
 
 # Render the Datadog config file
 mkdir /etc/datadog-agent
-cat <<EOM > /etc/datadog-agent/datadog.yaml
+cat <<-EOF > /etc/datadog-agent/datadog.yaml
 api_key: $KEY
-EOM
+EOF
 
 # Fetch and install the agent
 wget https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh -O /tmp/install_script.sh
 chmod +x /tmp/install_script.sh
 DD_API_KEY=$KEY DD_AGENT_MAJOR_VERSION=7 DD_INSTALL_ONLY=true /tmp/install_script.sh
-
-# To Dos:
-# Add tags in the agent config file
-# Install PostgreSQL and agent integration https://www.datadoghq.com/blog/collect-postgresql-data-with-datadog/
-# Create a custom agent check https://docs.datadoghq.com/developers/write_agent_check/?tab=agentv6v7
