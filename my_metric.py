79669006a4c02676c9279ce27d342916ce43f02e
daviddoiron2 <67924640+daviddoiron2@users.noreply.github.com>
2020-07-09 13:51:27 -0400

    Add files via upload

---
+++ my_metric.py
@@ -0,0 +1,12 @@
+import random
+
+from datadog_checks.base import AgentCheck
+
+__version__ = "1.0.0"
+
+class MyClass(AgentCheck):
+ def check(self, instance):
+  self.gauge(
+   "my_metric.gauge",
+   random.randint(0, 1000),
+   tags=["env:sandbox","metric_submission_type:gauge"])
