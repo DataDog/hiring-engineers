e3d0fccf2be37619213a081274dcda9bc3779e81
Matthew Glenn <mfglenn@outlook.com>
2021-01-26 12:51:31 -0700

    Add shell script to provision the VM with the datadog agent, and transfer the configuration files from the shared vagrant directory for MongoDB and the Custom Metric Check.

---
+++ environment/install_datadog.sh
@@ -0,0 +1,22 @@
+echo "Datadog Install & Configuration Script - Started"
+
+echo "Datadog Agent Install - Replace DD_API_KEY to change accounts"
+DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=d76729997c912addc8e121967b543a27 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+
+echo "Datadog Agent Configuration Move"
+sudo cp /vagrant/config/datadog.yaml /etc/datadog-agent/datadog.yaml 
+
+echo "Datadog MongoDB Integration Configuration Move"
+sudo cp /vagrant/config/conf.yaml /etc/datadog-agent/conf.d/mongo.d/conf.yaml
+
+echo "Datadog Custom Check - my_metric Configuration Move"
+sudo cp /vagrant/checks/custom_metric_check.py /etc/datadog-agent/checks.d/custom_metric_check.py
+sudo cp /vagrant/checks/custom_metric_check.yaml /etc/datadog-agent/conf.d/custom_metric_check.yaml
+
+echo "Execute Custom Check"
+sudo -u dd-agent -- datadog-agent check custom_metric_check
+
+echo "Restart Datadog Agent"
+sudo service datadog-agent restart
+
+echo "Datadog Install & Configuration Script - Completed"
