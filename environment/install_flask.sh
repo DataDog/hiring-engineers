5a74f2a5960abc5597fbdbba3d079a8787b628b0
Matthew Glenn <mfglenn@outlook.com>
2021-01-26 12:56:01 -0700

    Add shell script for python virtualenv, Flask, and ddtrace for reference.  Add requirement freeze for virtualenv.

---
+++ environment/install_flask.sh
@@ -0,0 +1,26 @@
+echo "Flask Install Script - Started"
+
+echo "Install pip"
+sudo apt update
+
+sudo apt install python3-pip
+
+pip3 --version
+
+echo "Install virtualenv"
+sudo apt-get install python3-venv
+
+echo "Create App Directory"
+mkdir flask_app
+
+cd flask_app
+
+echo "Create & Activate Python Virtual Environment"
+virtualenv venv
+
+. venv/bin/activate
+
+echo "Install Flask & ddtrace on Virtual Environment"
+#pip3 install -r requirements.txt
+
+echo "Flask Install Script - Completed"
