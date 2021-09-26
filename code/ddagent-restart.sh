0888bfae96587dab8ad3d1b6743c34f1cc78cff3
Jason Jakary <91029212+jasonjakary@users.noreply.github.com>
2021-09-26 11:06:08 -0400

    Update ddagent-restart.sh

--- code/ddagent-restart.sh
+++ code/ddagent-restart.sh
@@ -4,4 +4,4 @@
 systemctl restart datadog-agent
 
 # Scheduling is done by the below cron job:
-# @reboot sudo watch -n 45 /usr/bin/ddagent-restart.sh
+# @reboot sudo watch -n 45 /usr/local/bin/ddagent-restart.sh
