67ddbdc4c999e186cf2440b9a9dcee0656a68a50
Alex Sands <32316958+asands24@users.noreply.github.com>
2021-12-16 17:58:51 -0500

    Create my_metric.py
    check for my_metric

---
+++ my_metric.py
@@ -0,0 +1,16 @@
+
+# the following try/except block will make the custom check compatible with any Agent version
+try:
+    # first, try to import the base class from new versions of the Agent...
+    from datadog_checks.base import AgentCheck
+except ImportError:
+    # ...if the above failed, the check is running in Agent version < 6.6.0
+    from datadog_checks.base import AgentCheck
+
+# content of the special variable __version__ will be shown in the Agent status page
+__version__ = "1.0.0"
+
+class HelloCheck(AgentCheck):
+    def check(self, instance):
+        self.gauge('hello.world', [1000,1], tags=[test] + self.instance.get('tags', []))
+
