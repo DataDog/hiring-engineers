105b26031a442804081e9e61ea68fc8600fbd165
Gavin Whitlock <gavinswhitlock@gmail.com>
2022-03-23 11:00:47 -0500

    added rollup widget

--- dashboard.rb
+++ dashboard.rb
@@ -13,7 +13,32 @@ body = DatadogAPIClient::V1::Dashboard.new({
             type: DatadogAPIClient::V1::TimeseriesWidgetDefinitionType::TIMESERIES,
             requests: [
                 DatadogAPIClient::V1::TimeseriesWidgetRequest.new({
-                    q: "avg:my_metric{host:gavin-MacBookAir}"
+                    q: "my_metric{host:gavin-MacBookAir}"
+                })
+            ],
+            title:: "My Metric",
+            show_legend: false,
+            legend_size: 0
+        })
+    }),
+    DatadogAPIClient::V1::Widget.new({
+        definition: DatadogAPIClient::V1::TimeseriesWidgetDefinition.new({
+            type: DatadogAPIClient::V1::TimeseriesWidgetDefinitionType::TIMESERIES,
+            requests: [
+                DatadogAPIClient::V1::TimeseriesWidgetRequest.new({
+                    q: "sum:my_metric{host:gavin-MacBookAir}.rollup(sum,3600)",
+                    metadata: [
+                        {
+                            expression: "sum:my_metric{host:gavin-MacBookAir}.rollup(sum,3600)",
+                            alias_name: "my_metric_hourly_rollup"
+                        }
+                    ],
+                    display_type: "line",
+                    style: {
+                        palette: "dog_classic",
+                        line_type: "solid",
+                        line_width: "normal"
+                    }
                 })
             ]
         })
@@ -32,7 +57,7 @@ p api_instance.create_dashboard(body)
 #         aggregator: "avg",
 #         data_source:"metrics",
 #         name: "query1",
-#         query: "avg:my_metric(host:gavin-MacBookAir}"
+#         query: "my_metric(host:gavin-MacBookAir}"
 #     })
 # ]
 # })
